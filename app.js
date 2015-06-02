738e238a2853e0300d7471a36154e87201b20d2a
James Hughes <jh13293@my.bristol.ac.uk>
2015-06-02 13:28:53 +0100

    fixign static files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // respond with "Hello World!" on the homepage
 app.get('/', function (req, res) {
