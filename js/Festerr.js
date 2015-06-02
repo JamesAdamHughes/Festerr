e7a7f2dac1100c8dad0926ec71daf99e81d49584
James Hughes <jh13293@my.bristol.ac.uk>
2015-06-02 12:28:06 +0100

    search works proerly

--- js/Festerr.js
+++ js/Festerr.js
@@ -178,8 +178,13 @@ function showEventDetails(event_id, event_color){
 
 			//add color the json object
 			data["results"].eventcolor = event_color;
+			data["results"].eventcolor_light = shadeColor(event_color, 50);
 			data["results"].date = prettify_date(data["results"].date);
-			console.log(data["results"]);
+
+			if(data["results"].artists.length == 0){
+				data["results"].artists = [{"name" : "No artist data available for event"}];
+			}
+			// console.log(data["results"]);
 		    
 		    var rendered = Mustache.render(template, data["results"]);
 
@@ -477,4 +482,25 @@ function rgb2hsv () {
         s: Math.round(s * 100),
         v: Math.round(v * 100)
     };
+}
+
+function shadeColor(color, percent) {
+
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
 }
