db4e0ae17fb700bb17f300d2ae0fa3cd75e67d7d
James Hughes <jh13293@my.bristol.ac.uk>
2016-02-28 12:18:35 +0000

    Add build image query func to image search object

--- utils/googleImageSearch.js
+++ utils/googleImageSearch.js
@@ -8,7 +8,7 @@ var contents = fs.readFileSync(__dirname + '/../config/api_keys.json');
 var googleSearchAPI = JSON.parse(contents).google_search;
 
 // Set the params for the search
-var params = {
+var defaultParams = {
     q: "parklife+festival+2016",
     cx: googleSearchAPI.cx_key,
     key: googleSearchAPI.key,
@@ -19,30 +19,54 @@ var params = {
 };
 
 // Create the query from the params
-var query = queryBuilder.buildQuery(googleSearchAPI.url, params);
+// var query = queryBuilder.buildQuery(googleSearchAPI.url, params);
 
-// Make a request with the query
-// returns a promise resolved with the json of the search result
-// Or an object containing the error response
-function makeRequest(query) {
-    var deferred = q.defer();
-
-    request(query, function (err, res, body) {
-        if (!err && res.statusCode === 200) {
-            var searchResult = JSON.parse(body);
-            deferred.resolve(searchResult.items);
-        } else {
-            var body = JSON.parse(res.body);
-            deferred.reject(body.error);
-        }
-    });
+var googleImageSearch = {
     
-    return deferred.promise;
-}
+    // Builds a query string given a search term which can be used to make a request
+    buildImageQuery: function (searchTerm) {
+        var params = defaultParams;
+        var query = "";
+        var escapedSearch = "";
+        var searchTermList = searchTerm.split(" ");
+           
+        // Create a properly formatted search term, replacing whitespace with '+'
+        for(var i = 0; i < searchTermList.length; i++){
+            escapedSearch = escapedSearch + searchTermList[i] + "+";
+        }
+        escapedSearch = escapedSearch.substring(0, escapedSearch.length);
+        
+        params.q = escapedSearch;
+        query = queryBuilder.buildQuery(googleSearchAPI.url, params);
+        
+        return query;         
+    },
+    
+    // Make a request with the query
+    // returns a promise resolved with the json of the search result
+    // Or an object containing the error response
+    makeRequest: function (query) {
+        var deferred = q.defer();
+
+        request(query, function (err, res, body) {
+            if (!err && res.statusCode === 200) {
+                var searchResult = JSON.parse(body);
+                deferred.resolve(searchResult.items);
+            } else {
+                var body = JSON.parse(res.body);
+                deferred.reject(body.error);
+            }
+        });
+
+        return deferred.promise;
+    }
+};
 
 // Make a request
-makeRequest(query).then(function (res) {
-    console.log(res);
-}).catch(function (err) {
-    console.error(err);
-});
+// makeRequest(query).then(function (res) {
+//     console.log(res);
+// }).catch(function (err) {
+//     console.error(err);
+// });
+
+module.exports = googleImageSearch;
