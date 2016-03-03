e3c7470a80da4c84565f1e33325ed3dac26bb595
Louis Ditzel <louisditzel@gmail.com>
2016-03-03 11:41:24 +0000

    Cache event data on first request

--- controllers/events.js
+++ controllers/events.js
@@ -7,6 +7,8 @@ var fs = require('fs');
 // var contents = fs.readFileSync(__dirname + '/../config/api_keys.json');
 // var api_keys = JSON.parse(contents);
 
+var cachedEventData = null;
+
 // Serve requests to the event endpoint
 router.get('/event', function(req, res){
 
@@ -19,28 +21,37 @@ router.get('/event', function(req, res){
         // 'All' returns the entire events list
         if(req.query.type === "all") {
 
-            //Get all events from Skiddle API
-            var options = { method: 'GET',
-              url: process.env.skiddle_url  + 'events/',
-              qs: 
-               { eventcode: 'FEST',
-                 order: '4',
-                 api_key: process.env.skiddle_api_key } };
+            // Check to see if we've already cached the event data
+            if (!cachedEventData) {
+                //Get all events from Skiddle API
+                var options = { method: 'GET',
+                  url: process.env.skiddle_url  + 'events/',
+                  qs: 
+                   { eventcode: 'FEST',
+                     order: '4',
+                     api_key: process.env.skiddle_api_key } };
 
-            console.log("GET " + options.url);
+                console.log("GET " + options.url);
 
-            //Send the request
-            request(options, function (error, reqResponse, body) {
-              if (error) throw new Error(error);
-              var contents = JSON.parse(body);
-              if (contents.error !== 0) {
-                console.log('ERROR ' + contents.errorcode + ': ' + contents.errormessage);
-              } else {
-                  response = contents.results;
-                  response.ok = true;
-              }
-              res.send(response);
-            });
+                //Send the request
+                request(options, function (error, reqResponse, body) {
+                  if (error) throw new Error(error);
+                  var contents = JSON.parse(body);
+                  if (contents.error !== 0) {
+                    console.log('ERROR ' + contents.errorcode + ': ' + contents.errormessage);
+                  } else {
+                      cachedEventData = contents.results;
+                      response = contents.results;
+                      response.ok = true;
+                  }
+                  res.send(response);
+                });
+            // Cached data present
+            } else {
+                response = cachedEventData;
+                response.ok = true;
+                res.send(response);
+            }
 
         // Unknown query type
         } else {
