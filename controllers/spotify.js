436c957399f5c79c00fd67a37f5a511938cd8285
James Hughes <jh13293@my.bristol.ac.uk>
2016-03-05 16:38:50 +0000

    comment up the spotify code

--- controllers/spotify.js
+++ controllers/spotify.js
@@ -25,14 +25,21 @@ var generateRandomString = function (length) {
     return text;
 };
 
-// Serve requests to the event endpoint
+// Handle user wanting to auth with spotify
+// Requests auth token from spotity
+// Redirects user to spotify login page
 router.get('/spotifyLogin', function (req, res) {
 
+    console.log("GET https://accounts.spotify.com/authorize?");
+    console.log("GET /spotifyLogin");
+    
+    // Generate a random state
+    // The state can be useful for correlating requests and responses. Because your redirect_uri can
+    // be guessed, using a state value can increase your assurance that an incoming connection is the  
+    // result of an authentication request. 
     var state = generateRandomString(16);
     res.cookie(stateKey, state);
-    
-    console.log("GET https://accounts.spotify.com/authorize?");
-    
+        
     // your application requests authorization
     var scope = 'user-read-private user-read-email';
     res.redirect('https://accounts.spotify.com/authorize?' +
@@ -42,17 +49,19 @@ router.get('/spotifyLogin', function (req, res) {
             scope: scope,
             redirect_uri: redirect_uri,
             state: state
-        }));      
-    
+        }));
+
 });
 
-
+// Called by the spotify accounts service as the given redicted URI
+// Returns the auth code which we can use to ask for an access token
+// access token allows to to get user spotify information
 router.get('/spotifyCallback', function (req, res) {
-    
+
     console.log("GET /spotifyCallback");
+    
     // your application requests refresh and access tokens
     // after checking the state parameter
-
     var code = req.query.code || null;
     var state = req.query.state || null;
     var storedState = req.cookies ? req.cookies[stateKey] : null;
@@ -63,6 +72,8 @@ router.get('/spotifyCallback', function (req, res) {
                 error: 'state_mismatch'
             }));
     } else {
+        
+        // Request the access key using the auth code
         res.clearCookie(stateKey);
         var authOptions = {
             url: 'https://accounts.spotify.com/api/token',
@@ -76,7 +87,10 @@ router.get('/spotifyCallback', function (req, res) {
             },
             json: true
         };
-
+        
+        // Send the request, then redirect the auth code to our client browser
+        // TODO store the user access code
+        // TODO handle refreshing access codes
         request.post(authOptions, function (error, response, body) {
             if (!error && response.statusCode === 200) {
 
