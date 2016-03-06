09153e7dce6b97c5359f7ce117bebb7dffb75626
James Hughes <jh13293@my.bristol.ac.uk>
2016-03-06 14:21:46 +0000

    fix opening/closing dialog multiple times on click

--- public/app/views/header/header.js
+++ public/app/views/header/header.js
@@ -6,6 +6,8 @@ function HeaderController($scope, $mdDialog, $cookies, SpotifyService) {
 
     $scope.spotifyCodeExists = false;
     $scope.spotifyUserInfo = {};
+
+    var dialogOpen = false;
     
     // Handle user clicking the signup button Displays a signup dialogue
     // If spotify code exists don't need to do this again
@@ -13,14 +15,21 @@ function HeaderController($scope, $mdDialog, $cookies, SpotifyService) {
     var parentEl = angular.element(document.body);
     $scope.showSignupDialog = function ($event) {
 
+        if (!dialogOpen) {
+            dialogOpen = true;
             $mdDialog.show({
                 parent: parentEl,
                 clickOutsideToClose: true,
                 templateUrl: "app/views/header/signupDialog.html",
                 controller: SignupController,
                 // onComplete: afterShowAnimation,
+            }).finally(function(){
+                dialogOpen = false;
             });
-        // }
+        } else {
+            console.log("dialog already open");
+        }
+
     };
     
     // Check if spotify code exists, and get user info if true
@@ -28,12 +37,12 @@ function HeaderController($scope, $mdDialog, $cookies, SpotifyService) {
     if (accessCode) {
         console.log("already have a spotify code in cookies");
         $scope.spotifyCodeExists = true;
-        
-        SpotifyService.getUserInfo(accessCode).then(function(res){
+
+        SpotifyService.getUserInfo(accessCode).then(function (res) {
             console.log(res);
             $scope.spotifyUserInfo = res;
             $scope.spotifyUserInfo.short_name = res.display_name.split(" ")[0];
-        });      
+        });
     }
 }
 
