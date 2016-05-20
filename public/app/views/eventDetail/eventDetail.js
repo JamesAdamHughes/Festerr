f1089046703eaf852a2bfc7551e220689848c5cf
James Hughes <jh13293@my.bristol.ac.uk>
2016-05-20 11:48:55 +0100

    space out event icons a bit

--- public/app/views/eventDetail/eventDetail.js
+++ public/app/views/eventDetail/eventDetail.js
@@ -30,6 +30,9 @@ function EventDetailController($scope, $q, NetworkService, $location, SpotifySer
         // Get artist data then filter it
         NetworkService.callAPI(url, options).then(function(res) {
             if (res.ok) {
+                
+                console.log(res.event)
+                 
                 $scope.event = res.event;
                 $scope.eventLiked = res.liked ? true : false;
                 // filter the artists in the event 
