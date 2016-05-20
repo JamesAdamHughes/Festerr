53a2afb1add4e53cdc39939a9c0d27bc51026561
James Hughes <jh13293@my.bristol.ac.uk>
2016-05-20 11:58:58 +0100

    remove console log

--- public/app/views/eventDetail/eventDetail.js
+++ public/app/views/eventDetail/eventDetail.js
@@ -29,10 +29,7 @@ function EventDetailController($scope, $q, NetworkService, $location, SpotifySer
         
         // Get artist data then filter it
         NetworkService.callAPI(url, options).then(function(res) {
-            if (res.ok) {
-                
-                console.log(res.event)
-                 
+            if (res.ok) {                                 
                 $scope.event = res.event;
                 $scope.eventLiked = res.liked ? true : false;
                 // filter the artists in the event 
